df39ba356a8ffeb21beeb7b5c53eb950540850ed
heyBuddyCool <santhoshkumarpsk3000@gmail.com>
2025-02-12 23:45:51 +0530

    here i create additon operation file

---
+++ addTwoValues.java
@@ -0,0 +1,9 @@
+class addTwoValues{
+    public static void main (String args [] )
+    {
+        int number1=10;
+        int number2=20;
+        int result=number1+number2;
+        System.out.println("Answer is"+" "+result);
+    }
+}
